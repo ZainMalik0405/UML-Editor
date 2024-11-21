9d563bdb8984edf64dac819ca485c74515f5b8a1
ZainMalik0405 <zainmalikk0907@gmail.com>
2024-11-21 23:13:20 +0500

    Refactored package structure and added dependencies

---
+++ src/main/java/module-info.java
@@ -0,0 +1,18 @@
+module com.example.umleditor {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.umleditor to javafx.fxml;
+    exports com.example.umleditor;
+    exports com.example.umleditor.ui;
+    opens com.example.umleditor.ui to javafx.fxml;
+}
